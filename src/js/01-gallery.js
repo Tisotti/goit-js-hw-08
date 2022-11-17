68e7a2aeec142ab7afb649630eb29d5727d4f9af
Tisotti <yana.albertivna@gmail.com>
2022-11-17 01:23:55 +0100

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+
+const createImg = createImageGallery(galleryItems);
+
+listGallery.insertAdjacentHTML("beforeend", createImg);
+
+function createImageGallery(itemList) {
+  return itemList
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__item" href="${original}">
+          <img class="gallery__image"
+          src="${preview}"
+          alt="${description}" /> </a>`;
+    })
+    .join("");
+}
+new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
