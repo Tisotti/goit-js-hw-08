07e109a98e48e081fa685ae600b2e41802daf4b7
Tisotti <yana.albertivna@gmail.com>
2022-11-17 01:46:35 +0100

    2 task  start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+ const player = new Player(iframe);
+
+    player.on('timeupdate',Throttle( playTime,1000) );
+
+    function playTime({seconds}){   
+    localStorage.setItem("timeupdate",JSON.stringify(seconds));
+};
+    player.setCurrentTime(JSON.parse(localStorage.getItem('timeupdate')));
 
